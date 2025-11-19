21d5719b9f9ab4a657e02376b4db368c36303420
globnot <antoineborda-pro@proton.me>
2025-11-19 11:20:22 +0100

    Add lua mapping for resize windows

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,7 +1,20 @@
 require "nvchad.mappings"
 
 -- add yours here
---
+-- Fonction Lua pour redimensionner nvim-tree
+_G.resize_nvim_tree = function(delta)
+    for _, win in pairs(vim.api.nvim_list_wins()) do
+        local buf = vim.api.nvim_win_get_buf(win)
+        if vim.bo[buf].filetype == "NvimTree" then
+            local width = vim.api.nvim_win_get_width(win)
+            vim.api.nvim_win_set_width(win, width + delta)
+            break
+        end
+    end
+end
+-- Redimensionner nvim-tree dynamiquement
+vim.api.nvim_set_keymap('n', '<leader>+', ':lua resize_nvim_tree(5)<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>-', ':lua resize_nvim_tree(-5)<CR>', { noremap = true, silent = true })
 -- Copier dans le presse-papiers système
 vim.keymap.set('n', 'y', '"+y', { noremap = true, silent = true })
 vim.keymap.set('n', 'yy', '"+yy', { noremap = true, silent = true })
